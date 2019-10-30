dac804a4ff0c33a81affbe7031d29dcffb3383bb
Aditi Mohanty <rheaditi@users.noreply.github.com>
2019-10-30 14:23:52 +0530

    Fix: post-checout tests
    ---
    [#168690318]

--- src/utils/hooks.test.ts
+++ src/utils/hooks.test.ts
@@ -1,22 +1,38 @@
 import { shouldSkipBranchCheck } from './hooks';
+import { inDetachedHeadState } from '../utils/git';
+
+jest.mock('../utils/git', () => ({
+  inDetachedHeadState: jest.fn(),
+}));
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  jest.resetAllMocks();
+});
 
 it('should not skip when checking out a branch', () => {
   // git checkout bugfix/post-checkout-fails-when-checking_168659719
+  (inDetachedHeadState as jest.Mock).mockReturnValue(false);
   expect(
     shouldSkipBranchCheck('61c0ee6200a3f9faeb1e9c24755861582384c1a0', '4c51aece308a74b4890ff67bbb2a0f75379ffd63', '1')
-  ).toBeFalsy();
+  ).toBe(false);
+  expect(inDetachedHeadState).toHaveBeenCalledTimes(1);
 });
 
 it('should skip when checking out a SHA', () => {
   // git checkout 8da0bfffc570e8f2ad01cf91dfe1b124994fa068
+  (inDetachedHeadState as jest.Mock).mockReturnValue(true);
   expect(
     shouldSkipBranchCheck('8da0bfffc570e8f2ad01cf91dfe1b124994fa068', '8da0bfffc570e8f2ad01cf91dfe1b124994fa068', '1')
   ).toBeTruthy();
+  expect(inDetachedHeadState).toHaveBeenCalledTimes(1);
 });
 
 it('should skip when checking out a file', () => {
   // git checkout origin/master post-checkout.js
+  (inDetachedHeadState as jest.Mock).mockReturnValue(false);
   expect(
-    shouldSkipBranchCheck('472c1137ca2bc3da76f83fdd1b665e9ada4e8b1b', '472c1137ca2bc3da76f83fdd1b665e9ada4e8b1b', '1')
+    shouldSkipBranchCheck('472c1137ca2bc3da76f83fdd1b665e9ada4e8b1b', '472c1137ca2bc3da76f83fdd1b665e9ada4e8b1b', '0')
   ).toBeTruthy();
+  expect(inDetachedHeadState).not.toHaveBeenCalled();
 });
